88222df3bfa8a28d653ce756b4aecd844d0356b6
Akhil Acharya <akhilcacharya@gmail.com>
2016-08-23 22:37:03 -0400

    Add Scalatra base project

---
+++ irankerg/README.md
@@ -0,0 +1,12 @@
+# irankerg #
+
+## Build & Run ##
+
+```sh
+$ cd irankerg
+$ ./sbt
+> jetty:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ irankerg/src/main/scala/com/irankerg/app/servelet.scala
@@ -0,0 +1,16 @@
+package com.irankerg.app
+
+import org.scalatra._
+
+class servelet extends IrankergStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
